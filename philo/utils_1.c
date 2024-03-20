ea60569cc76ae7acb0ecbdcc63da93d3240655cf
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-03-20 19:31:15 +0400

    minor changes

--- philo/utils_1.c
+++ philo/utils_1.c
@@ -6,7 +6,7 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 13:26:13 by akeryan           #+#    #+#             */
-/*   Updated: 2024/03/20 19:25:21 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/03/20 19:27:03 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,14 +45,3 @@ int	error_msg(char *str, t_data *data)
 		destroy(data);
 	return (1);
 }
-
-//int	alert_sleeping(LONG sleep_time)
-//{
-	//while (get_time() < sleep_time)
-	//{
-		//usleep(100);
-		//if (get_time() >= philo->time_to_die)
-			//change_state(DIED, philo);
-	//}
-
-//}
